fb32bf82ef7e3846fe2073ebf94172c20cf52670
BurAndBY <48630651+BurAndBY@users.noreply.github.com>
2020-11-16 20:55:30 +0300

    Add files via upload

--- src/lang/index.js
+++ src/lang/index.js
@@ -6,12 +6,13 @@ import ja from './TranslationsJA';
 import fr from './TranslationsFR';
 import cs from './TranslationsCS';
 import de from './TranslationsDE';
+import ru from './TranslationsRU';
 
 Vue.use(VueI18n);
 
 const translations = Object.assign(en, ja, fr, cs, de);
 
-const languages = ['en', 'ja', 'fr', 'cs', 'de'];
+const languages = ['en', 'ja', 'fr', 'cs', 'de', 'ru'];
 
 export function checkLanguage(language) {
     return navigator.language.split('-')[0] == language;
